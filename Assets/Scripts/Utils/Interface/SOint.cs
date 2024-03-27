34b62fd56569a3ae5714a10339d2f62080f5f2d4
Eryck Kawa <okazakiall114@gmail.com>
2024-03-26 22:07:51 -0300

    add scriptable objects int UI coins

---
+++ Assets/Scripts/Utils/Interface/SOint.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ebac-platformer2d-study/SOint")]
+public class SOint : ScriptableObject
+{
+    public int value;
+}
