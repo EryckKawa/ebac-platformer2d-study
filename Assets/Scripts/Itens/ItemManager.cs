7953f9ce7e1d71591f32324d5ef6a298746aaac7
Eryck Kawa <okazakiall114@gmail.com>
2024-03-26 21:08:59 -0300

    add ui coins text in the project

--- Assets/Scripts/Itens/ItemManager.cs
+++ Assets/Scripts/Itens/ItemManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Core.Singleton;
+using TMPro;
 
 //Cria o singleton ItemManager.
 public class ItemManager : Singleton<ItemManager>
 {
     // Variável para armazenar a quantidade de moedas
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
 
     // Start é chamado antes do primeiro frame
     private void Start()
@@ -21,6 +23,7 @@ public class ItemManager : Singleton<ItemManager>
     {
         // Define a quantidade inicial de moedas como zero
         coins = 0;
+        UpdateCoins();
     }
 
     // Método para adicionar uma quantidade específica de moedas
@@ -28,5 +31,10 @@ public class ItemManager : Singleton<ItemManager>
     {
         // Adiciona a quantidade especificada à variável coins
         coins += amount;
+        UpdateCoins();
+    }
+    void UpdateCoins()
+    {
+        UIInGameManager.UpdateTextCoins(coins.ToString());
     }
 }
