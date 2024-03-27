07ce28bcfb6053c2077969ebd8c0133559d54196
Eryck Kawa <okazakiall114@gmail.com>
2024-03-27 01:02:15 -0300

    add scriptable object int and float

---
+++ Assets/Scripts/Utils/SO/SOfloat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ebac-platformer2d-study/SOfloat")]
+public class SOfloat : ScriptableObject
+{
+    public float value;
+}
+
